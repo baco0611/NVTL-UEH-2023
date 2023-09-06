5de6d5cdf73c436aaf25aa2b29e75eb0b4dfd4d3
vananh0611 <ntvanh2003a2@gmail.com>
2023-09-06 13:40:07 +0700

    [FIX] Fix casting admin

--- app/Http/Controllers/Api/CollaboratorsController.php
+++ app/Http/Controllers/Api/CollaboratorsController.php
@@ -42,9 +42,9 @@ class CollaboratorsController extends Controller
             if ($key=='image'){
                 $folderPath = public_path() . '/media/' . 'CTV/image/';
             }
-            if ($key=='video'){
-                $folderPath = public_path() . '/media/' . 'CTV/video/';
-            }
+            // if ($key=='video'){
+            //     $folderPath = public_path() . '/media/' . 'CTV/video/';
+            // }
             if ($key=='application'){
                 $folderPath = public_path() . '/media/' . 'CTV/text/';
             }
